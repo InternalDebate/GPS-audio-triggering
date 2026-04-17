88eeabdb9ef3cc0f1ff684b1ddbde38b001e8c72
InternalDebate <gamemailbox1000@gmail.com>
2026-04-17 09:30:39 +0200

    added more zones, linked sound files
    linked soundfiles to the zones, and added some more zones

--- script.js
+++ script.js
@@ -21,14 +21,16 @@ console.log = function (...args) {
 
 // --- 1. DATA ---
 const soundZones = [
-    { name: "maliebaan 1", lat: 52.088679, lng: 5.129786, radius: 10, audioFile: 'Nature noise - Echoes - EMF.mp3' },
-    { name: "maliebaan 13", lat: 52.089120, lng: 5.130747, radius: 10, audioFile: 'Nature noise - Echoes - PIEP.mp3' },
-    { name: "electriciteitskast", lat: 52.089430, lng: 5.130836, radius: 7, audioFile: 'Nature noise - Echoes - WATERLEIDING.mp3' },
-    { name: "maliebaan 14", lat: 52.090412, lng: 5.131557, radius: 5, audioFile: 'Nature noise - Echoes - DUIF.mp3' },
-    { name: "maliebaan 16", lat: 52.090561, lng: 5.131788, radius: 7, audioFile: 'Nature noise - Echoes - EMF.mp3' },
-    { name: "maliebaan 16 poort", lat: 52.090715, lng: 5.131978, radius: 5, audioFile: 'Nature noise - Echoes - EMF.mp3' },
-    { name: "maliebaan 24 charging station", lat: 52.090992, lng: 5.132597, radius: 10, audioFile: 'Nature noise - Echoes - EMF.mp3' },
-    { name: "maliebaan voorbijgaande auto's", lat: 52.089994, lng: 5.131597, radius: 20, audioFile: 'Nature noise - Echoes - EMF.mp3' },
+    { name: "maliebaan 1", lat: 52.088679, lng: 5.129786, radius: 10, audioFile: 'Nature noise - EMF 1.mp3' },
+    { name: "maliebaan 13", lat: 52.089120, lng: 5.130747, radius: 10, audioFile: 'Nature noise - EMF 2.mp3' },
+    { name: "electriciteitskast", lat: 52.089430, lng: 5.130836, radius: 7, audioFile: 'Nature noise - EMF 3' },
+    { name: "maliebaan 14", lat: 52.090412, lng: 5.131557, radius: 5, audioFile: 'Nature noise - EMF 4 - phones & chips.mp3' },
+    { name: "maliebaan 143 charging station", lat: 52.093790, lng: 5.137581, radius: 7, audioFile: 'Nature noise - EMF 4 - phones & chips.mp3' },
+    { name: "maliebaan vrijmetselaarsloge", lat: 52.093349, lng: 5.135959, radius: 12, audioFile: 'Nature noise - EMF 6 - headphone conus.mp3' },
+    { name: "maliebaan 24 charging station", lat: 52.090992, lng: 5.132597, radius: 10, audioFile: 'Nature noise - EMF 5.mp3' },
+    { name: "maliebaan voorbijgaande auto's", lat: 52.089994, lng: 5.131597, radius: 20, audioFile: 'Nature noise - EMF 7 - Autos.mp3' },
+    { name: "maliebaan duif", lat: 52.091572, lng: 5.133955, radius: 20, audioFile: 'Nature noise - Vogel - Duif.mp3' },
+    { name: "maliebaan mus", lat: 52.089168, lng: 5.129830, radius: 20, audioFile: 'Nature noise - Vogel - mus.mp3' },
 ];
 
 // --- 2. MAP SETUP ---
